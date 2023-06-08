7dfe09875c9fc87606fcd7f2067434b4b5d0a0f6
DanielloXXI <cybiran_ez@mail.ru>
2023-06-08 18:04:55 +0300

    Поменял errorlogger местами

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,7 @@ app.get('/crash-test', () => {
   }, 0);
 });
 app.use('/', router);
+app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
@@ -46,7 +47,6 @@ app.use((err, req, res, next) => {
     });
   next();
 });
-app.use(errorLogger);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
